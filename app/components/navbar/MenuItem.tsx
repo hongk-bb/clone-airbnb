19cbc361136c66d05987e7893fb947e08452853e
cfy <732001734@qq.com>
2023-06-06 19:10:54 +0800

    ✨ feat: sign up form

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -11,6 +11,7 @@ const MenuItem: React.FC<MenuItemProps> = ({
 }) => {
   return (
     <div
+      onClick={onClick} 
       className="
         px-4
         py-3
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import { Nunito } from "next/font/google"
 
 import "./globals.css"
 import Navbar from "./components/navbar/Navbar"
+import ClientOnly from "./components/ClientOnly"
+import RegisterModal from "./components/modals/RegisterModal"
+import ToasterProvider from "./providers/ToasterProvider"
 
 const font = Nunito({
   subsets: ["latin"]
@@ -20,7 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+
         {children}
       </body>
     </html>
